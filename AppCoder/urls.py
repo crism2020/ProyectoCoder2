0d0d69bc8694a8f4325e19a31f9287f0871eb14e
crism2020 <cristianmurray@hotmail.com>
2023-03-30 12:24:59 -0300

    Cambios realizados con Presentacion clase 20 OK

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -3,11 +3,11 @@ from AppCoder import views
 
 urlpatterns =[
 
-    path ("", views.inicio),
+    path ("", views.inicio, name="Inicio"),
     path ("cursos", views.cursos, name="Cursos"),
-    path ("profesores", views.profesores),
-    path ("estudiantes", views.estudiantes),
-    path ("estudiantes", views.entregables)
+    path ("profesores", views.profesores, name="Profesores"),
+    path ("estudiantes", views.estudiantes, name="Estudiantes"),
+    path ("entregables", views.entregables, name="Entregables"),
 
 
 ]
